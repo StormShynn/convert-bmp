9579f6db5f685a3c994259d0c90599890bfd52b5
StormShyn <94594049+StormShynn@users.noreply.github.com>
2023-07-13 11:55:06 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 // Get references to the necessary elements
-const imageInput = document.getElementById('imageInput');
-const convertButton = document.getElementById('convertButton');
+const imageInput = document.getElementById("imageInput");
+const convertButton = document.getElementById("convertButton");
 
 // Add a listener to the convert button
-convertButton.addEventListener('click', convertTo8BitBMP);
+convertButton.addEventListener("click", convertTo8BitBMP);
 
 // Function to handle the conversion process
 function convertTo8BitBMP() {
   const file = imageInput.files[0];
   if (!file) {
-    alert('Please select a file.');
+    alert("Please select a file.");
     return;
   }
 
   const reader = new FileReader();
-  reader.onload = function(event) {
+  reader.onload = function (event) {
     const img = new Image();
-    img.onload = function() {
+    img.onload = function () {
       // Convert the image to an 8-bit BMP format using UPNG.js
-      const bmpData = UPNG.encode([img], img.width, img.height, 256, [0, 1, 2, 3]);
+      const bmpData = UPNG.encode(
+        [img],
+        img.width,
+        img.height,
+        256,
+        [0, 1, 2, 3]
+      );
 
-      // Create a Blob object from the BMP data
-      const blob = new Blob([bmpData], { type: 'image/bmp' });
+      // Compress the BMP data using pako
+      const compressedData = pako.deflate(bmpData);
 
-      // Create a temporary anchor element to trigger the download
-      const downloadLink = document.createElement('a');
+      // Create a Blob object from the compressed data
+      const blob = new Blob([compressedData], { type: "image/bmp" });
+
+      // Generate a temporary download link
+      const downloadLink = document.createElement("a");
       downloadLink.href = URL.createObjectURL(blob);
-      downloadLink.download = 'converted_image.bmp';
-      document.body.appendChild(downloadLink);
+      downloadLink.download = "converted_image.bmp";
 
       // Programmatically trigger the download
+      document.body.appendChild(downloadLink);
       downloadLink.click();
-
-      // Remove the temporary anchor element
       document.body.removeChild(downloadLink);
     };
 
